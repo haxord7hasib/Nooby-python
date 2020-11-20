8a6c99e1246296139330d6b3ed2aa2cecf1186e8
haxord7-Hasib <haxord7@gmail.com>
2020-11-20 22:56:26 +0600

    Create userlogin.py

---
+++ userlogin.py
@@ -0,0 +1,16 @@
+x=4
+z=4 
+name="Hasib"
+password="awse2313"
+print("Username:")
+user = input()
+print("Welcome back\t"+str(user))
+
+if user==name:
+    print("Password:")
+    password=input()
+    if password==password:
+     print("Login successful\t "+str(user))
+  
+else:
+  print("Invalid user")  
